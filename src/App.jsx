50817473d3ae44de893091749131c9b4b6074ec7
Roy <rjperlman@gmail.com>
2024-12-02 11:53:08 -0800

    Got some files

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,35 @@
-/*import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'*/
+import {puppyList} from './data.js'
+import {useState} from 'react'
+//import './path-to-css.css'
 
-function App() {
- //const [count, setCount] = useState(0)
+function App() 
+{
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
 
-  return (
-    <>
-      <div>
-        
-        
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  console.log(puppyList);
+  console.log("puppyList: ", puppies);
+  console.log(featuredPup);
+
+return (
+  <>
+    <div>
+      {puppies.map((puppy) => (
+        <p onClick={() => { setFeatPupId(puppy.id); }} key={puppy.id}> {puppy.name} </p>
+      ))}
+    { featPupId && featuredPup && (
+          <div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
+    </div>
+  </>
+);
 }
 
-/**
- * <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
- */
 export default App
